bfccd586aa63bc8289b168c3202ee8eada8726a4
alaahossamelsaeed <163353424+alaahossamelsaeed@users.noreply.github.com>
2024-08-22 17:58:01 +0300

    update account screen

--- app/lib/Accountscreen.dart
+++ app/lib/Accountscreen.dart
@@ -10,147 +10,241 @@ class AccountScreen extends StatelessWidget {
     final screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text(
-          'My Account',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: const Text(
+            'My Account',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Profile
-              Row(
-                children: [
-                  CircleAvatar(
-                    radius: screenWidth * 0.09,
-                    backgroundImage: const AssetImage(
-                        'assets/images/Yellow Gradient Facebook Profile Picture.png'),
-                  ),
-                  SizedBox(
-                    width: screenWidth * 0.04,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Username: $username',
-                        style: const TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-              SizedBox(height: screenHeight * 0.1),
-
-              /////////////////SETTING PAGE////////////////////////
-              Text(
-                'Settings',
-                style: TextStyle(
-                    fontWeight: FontWeight.w600, fontSize: screenWidth * 0.035),
-              ),
-              SizedBox(height: screenHeight * 0.035),
-              Column(
-                children: [
-                  ListTile(
-                    leading: const Icon(Icons.shopping_cart),
-                    title: const Text('My Orders'),
-                    onTap: () {
-                      /////////////////ORDERS PAGE////////////////////////
-                    },
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.account_circle),
-                    title: const Text('Account Settings'),
-                    onTap: () {
-                      /////////////////ACCOUNT SETTING PAGE////////////////////////
-                    },
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.notifications),
-                    title: const Text('Notification Settings'),
-                    onTap: () {
-                      /////////////////Notification SETTING PAGE////////////////////////
-                    },
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.info),
-                    title: const Text('About Us'),
-                    onTap: () {
-                      /////////////////ABOUT US PAGE////////////////////////
-                    },
-                  ),
-                ],
-              ),
-
-              // Buttons
-              SizedBox(height: screenHeight * 0.1),
-              Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Profile
+                Row(
                   children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        ///////////////DELETE MY ACCOUNT/////////////////
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        padding: EdgeInsets.symmetric(
-                          horizontal: screenWidth * 0.122,
-                          vertical: screenHeight * 0.02,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                      ),
-                      child: const Text(
-                        'Delete My Account',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.red,
-                        ),
-                      ),
+                    CircleAvatar(
+                      radius: screenWidth * 0.09,
+                      backgroundImage: const AssetImage(
+                          'assets/images/Yellow Gradient Facebook Profile Picture.png'),
                     ),
-                    SizedBox(height: screenHeight * 0.02),
-                    ElevatedButton(
-                      onPressed: () {
-                        ///////////////LOG OUT/////////////////
+                    SizedBox(
+                      width: screenWidth * 0.04,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Username: $username',
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(height: screenHeight * 0.1),
+
+                //setting
+                Text(
+                  'Settings',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: screenWidth * 0.035),
+                ),
+                SizedBox(height: screenHeight * 0.035),
+                Column(
+                  children: [
+                    ListTile(
+                      leading: const Icon(Icons.shopping_cart),
+                      title: const Text('My Orders'),
+                      onTap: () {
+                        /////////////////ORDERS PAGE////////////////////////
                       },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xfff49c63),
-                        padding: EdgeInsets.symmetric(
-                          horizontal: screenWidth * 0.2,
-                          vertical: screenHeight * 0.02,
+                    ),
+                    ExpansionTile(
+                      leading: const Icon(Icons.settings),
+                      title: const Text('Account Settings'),
+                      children: <Widget>[
+                        ListTile(
+                          title: const Text('Theme'),
+                          onTap: () {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: const Text('Choose Theme'),
+                                  content: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: <Widget>[
+                                      ListTile(
+                                        title: const Text('Light'),
+                                        onTap: () {
+                                          // Set theme to light
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                      ListTile(
+                                        title: const Text('Dark'),
+                                        onTap: () {
+                                          // Set theme to dark
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                      ListTile(
+                                        title: const Text('System Default'),
+                                        onTap: () {
+                                          // Set theme to system default
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              },
+                            );
+                          },
                         ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
+                        ListTile(
+                          title: const Text('Language'),
+                          onTap: () {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: const Text('Choose Language'),
+                                  content: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: <Widget>[
+                                      ListTile(
+                                        title: const Text('English'),
+                                        onTap: () {
+                                          // Set language to English
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                      ListTile(
+                                        title: const Text('Spanish'),
+                                        onTap: () {
+                                          // Set language to Spanish
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                      ListTile(
+                                        title: const Text('French'),
+                                        onTap: () {
+                                          // Set language to French
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              },
+                            );
+                          },
                         ),
-                      ),
-                      child: const Text(
-                        'Log Out',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.white,
+                      ],
+                    ),
+
+                    ExpansionTile(
+                      leading: const Icon(Icons.notifications),
+                      title: const Text('Notification Settings'),
+                      children: <Widget>[
+                        ListTile(
+                          title: const Text('Enable Notifications'),
+                          onTap: () {
+                            // Handle enabling notifications
+                          },
                         ),
+                      ],
+                    ),
+                    ExpansionTile(
+                      leading: const Icon(Icons.info),
+                      title: const Text('About Us'),
+                      children: <Widget>[
+                        ListTile(
+                          title: const Text('Contact Us'),
+                          onTap: () {
+                            // Navigate to or display the "Contact Us" page
+                          },
+                        ),
+                        ListTile(
+                          title: const Text('Privacy Policy'),
+                          onTap: () {
+                            // Navigate to or display the "Privacy Policy" page
+                          },
+                        ),
+                      ],
+                    ),
+
+                    // Buttons
+                    SizedBox(height: screenHeight * 0.1),
+                    Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              ///////////////DELETE MY ACCOUNT/////////////////
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.white,
+                              padding: EdgeInsets.symmetric(
+                                horizontal: screenWidth * 0.122,
+                                vertical: screenHeight * 0.02,
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                            ),
+                            child: const Text(
+                              'Delete My Account',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.red,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: screenHeight * 0.02),
+                          ElevatedButton(
+                            onPressed: () {
+                              ///////////////LOG OUT/////////////////
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xfff49c63),
+                              padding: EdgeInsets.symmetric(
+                                horizontal: screenWidth * 0.2,
+                                vertical: screenHeight * 0.02,
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                            ),
+                            child: const Text(
+                              'Log Out',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ],
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
